fd0c4a5fc7542284bbc75ed76791bd8dfcac53ef
Kenan Babajic <BabKe869@school.lu>
2023-10-06 11:45:35 +0200

    sasa

--- Content/Script.js
+++
@@ -1,9 +0,0 @@
-const Values = ["Home", "About", "Contact", "Products", "Members"];
-
-function Choose() {
-    let Nav = document.getElementByClassName("topnav");
-    console.log(Nav)
-    console.log("Bruh")
-
-    Nav.classList.add("Bruh")
-}
