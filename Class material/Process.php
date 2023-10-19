c8d79328d35e0d559968fbc6b1398d19d3f0bc46
Kenan Babajic <BabKe869@school.lu>
2023-10-19 14:23:13 +0200

    3ds

--- Class material/Process.php
+++ Class material/Process.php
@@ -9,14 +9,9 @@
 <body>
     <h1>Welcome 
     <?php
-print($_GET["UsersName"]);
-print("<br>");
-$Equation = ($_GET["Equation"]);
-$Equation2 = ($_GET["Equation2"]);
+
 // $Equation = (int)($_GET["Equation"]); - if we use input type text
 // $Equation = (int)($_GET["Equation2"]); - if we use input type text
-
-print("The result is " . $Equation + $Equation2);
 
 ?>
 
