8cc3362fd500778e63ef44b0d3ed4321d16ef472
Kenan Babajic <BabKe869@school.lu>
2023-10-19 14:23:40 +0200

    dfgf

--- Class material/Userinput.php
+++ Class material/Userinput.php
@@ -9,17 +9,54 @@
 </head>
 
 <body>
+    <?php
+$a = 30;
 
-    <form method="GET" action="Process.php">
-        Please enter your name:
+if(isset($a)) {
+    print("The variable exists");
 
-        <input type="text" name="UsersName">
-        <br>
+} else {
+    print("The value is not set");
+}
+
+if (isset($_GET["XAdd"], $_GET["YAdd"])) { //checks if they are set
+    if(is_numeric($_GET["XAdd"]) && is_numeric($_GET["YAdd"])) { //checks if they are numbers
+    ?>
+    <h1><?=(int)$_GET["XAdd"] ?> + <?= (int)$_GET["YAdd"] ?> = <?= (int)$_GET["XAdd"] + (int)$_GET["YAdd"] ?></h1>
+    <?php
+}
+}
+if (isset($_GET["XSubtract"], $_GET["YSubtract"])){
+    if(is_numeric($_GET["XSubtract"]) && is_numeric($_GET["YSubtract"])) { //checks if they are numbers
+    ?>
+    <h1><?=(int)$_GET["XSubtract"] ?> - <?= (int)$_GET["YSubtract"] ?> = <?= (int)$_GET["XSubtract"] - (int)$_GET["YSubtract"] ?></h1>
+    <?php
+    }
+}
+    ?>
+    ADD
+    <form method="GET">
         Please enter 2 numbers:
-        <input type="number" name="Equation">
-        <input type="number" name="Equation2">
+        <input type="number" name="XAdd">
+        <input type="number" name="YAdd">
         <input type="submit" value="Submit">
     </form>
+    SUBTRACT
+    <form method="GET">
+        Please enter 2 numbers:
+        <input type="number" name="XSubtract">
+        <input type="number" name="YSubtract">
+        <input type="submit" value="Submit">
+    </form>
+<?php
+/* Isset in php
+
+is an instruction that checks if a variable has bee nset or not
+*/
+
+// EXERCISE : Create a single PHP Page with a button inside a form that when it's clicked will disappear
+?>
+
 </body>
 
 </html>
