02bf7e0cc6babb204a98d8d1b217d21e9f8be76f
Kenan Babajic <BabKe869@school.lu>
2024-03-12 14:22:49 +0100

    uidgfgfdgdf

--- Class material/SQL/FirstPhpDbConnect.php
+++ Class material/SQL/FirstPhpDbConnect.php
@@ -9,11 +9,28 @@
 <body>
     <?php
 
-    //GOAL 1: connect to the database
-    $connection = new mysqli("localhost","root","","Test2024"); // Connect to the Database
+error_reporting(0); // To disable the rusty ahh errors
+// $connection = new mysqli("localhost","root","","Test2024") or die("Database is closed"); // Connect to the Database
 
+    //GOAL 1: connect to the database
+    mysqli_report(MYSQLI_REPORT_OFF);
+
+$connection = mysqli_connect("localhost","root","","Test2024");
+
+ if (!$connection) {
+    die("Error creating connection");
+ }
+
+   /* $connection = @mysqli_connect("localhost","root","","Test2024");
+    }catch (Exception $e) {
+        die("Error occured while connecting to the database");
+    }*/
+    
     //GOAL 2: select data from my table of clients and display it.
     $sqlQuery = $connection->prepare("SELECT * from Clients"); // Create a query
+    if(!$sqlQuery) {
+        die("Error creating the sql query");
+    }
     $sqlQuery->execute(); // Execute the query
     $result = $sqlQuery->get_result(); // Get the result back from the Database
     while($row=$result->fetch_assoc()) { // Get result from each line
